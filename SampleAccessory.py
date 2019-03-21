2d05876e5108885ba576ffc659168836da511843
Robert Benjamin Osborne <Robert@Roberts-Air-2.domain>
2019-03-20 23:37:03 -0400

    Create basic homekit accessory

--- SampleAccessory.py
+++ SampleAccessory.py
@@ -1,23 +1,40 @@
 #!/usr/bin/env python3
 
 import os.path
+import logging
 
-from homekit import HomeKitServer
+
+from homekit import AccessoryServer
 from homekit.model import Accessory, LightBulbService
+from homekit.model.characteristics import BrightnessCharacteristic, HueCharacteristic
 
 
 if __name__ == '__main__':
-    try:
-        httpd = HomeKitServer(os.path.expanduser('~/.homekit/demoserver.json'))
+    # setup logger
+    logger = logging.getLogger('accessory')
+    logger.setLevel(logging.INFO)
+    ch = logging.StreamHandler()
+    ch.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    logger.addHandler(ch)
+    logger.info('starting')
 
-        accessory = Accessory('Licht')
+    try:
+        httpd = AccessoryServer(os.path.expanduser('./demoserver.json'), logger)
+
+        accessory = Accessory('Light', 'PheonixPhi', 'Demoserver', '0001', '0.1')
+
         lightService = LightBulbService()
+        # lightService.append_characteristic(BrightnessCharacteristic(12345678901))
+        lightService.append_characteristic(HueCharacteristic(12345678902))
         accessory.services.append(lightService)
         httpd.accessories.add_accessory(accessory)
 
         httpd.publish_device()
         print('published device and start serving')
+
         httpd.serve_forever()
+
+
     except KeyboardInterrupt:
         print('unpublish device')
         httpd.unpublish_device()
